2bc324be212fc749d133add2f19f16cd955558b5
TechQuery <shiy2008@gmail.com>
2025-03-22 01:21:08 +0800

    [add] Event class polyfill

--- source/utility.ts
+++ source/utility.ts
@@ -2,28 +2,7 @@ import { ProgressData, ProgressEventTarget } from 'koajax';
 import { ReadableStream } from 'web-streams-polyfill';
 import { createAsyncIterator } from 'web-utility';
 
-export function polyfillProgressEvent() {
-  return (globalThis.ProgressEvent ||= class ProgressEvent<
-    T extends EventTarget = EventTarget
-  > extends Event {
-    declare target: T | null;
-
-    lengthComputable: boolean;
-    total: number;
-    loaded: number;
-
-    constructor(
-      type: string,
-      { lengthComputable, total, loaded, ...meta }: ProgressEventInit = {}
-    ) {
-      super(type, meta);
-
-      this.lengthComputable = lengthComputable;
-      this.total = total;
-      this.loaded = loaded;
-    }
-  });
-}
+import { ProgressEvent } from './Event';
 
 export const streamFromProgress = <T extends ProgressEventTarget>(target: T) =>
   createAsyncIterator<ProgressData, ProgressEvent<T>>(
@@ -48,8 +27,6 @@ export async function* emitStreamProgress(
   eventTarget: ProgressEventTarget
 ): AsyncGenerator<Uint8Array> {
   var loaded = 0;
-
-  polyfillProgressEvent();
 
   for await (const chunk of stream) {
     yield chunk;
